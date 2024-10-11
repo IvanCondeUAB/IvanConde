8ae80901379eeab477052120ea91832e7c8b0aaa
IvanCondeUAB <icondesantolaya@gmail.com>
2024-10-11 16:38:40 +0200

    Create EXERCICIS 4 PAI.js

---
+++ EXERCICIS 4 PAI.js
@@ -0,0 +1,64 @@
+//Exercici 4.1
+let a = 25; 
+let b = 15 + a; 
+let c = b * 2; 
+console.log(a,b,c);
+//S'escriuran els valors a=25, b=40, c=80
+
+
+
+//Exercici 4.2
+let a = 10; 
+let b = 20; 
+let c = 5; 
+a = a + 3; 
+b = b + 4 - a; 
+c = a + b + c; 
+a = a + c; 
+b = 4; 
+c = c + 3 - b + 2; 
+console.log(a,b,c);
+//S'escriuran els valors a=42, b=4, c=30
+
+
+
+//Exercici 4.3
+//Quins valors s'escriuran a la consola després d'executar aquest codi? 
+//Escriu-los en un paper i desprès executa el codi. Et donen el mateix?
+let a = 5; 
+let b = 18; 
+let c = 15; 
+let d = 22; 
+a = a + 10; 
+b = b + 5 - c; 
+c = c + 4 + b; 
+d = d + b + a; 
+a = a + 1; 
+b = b + c; 
+c = b + c; 
+d = b + b;
+//A la consola s'escriu el valor 70
+
+
+
+//Exercici 4.4
+//Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer 
+//sobre paper i desprès prova a l’ordinador. 
+let a = 10 
+let b = 20 
+let c = 5 
+a = a + 3 // Fixat en aquesta instrucció.  
+// Quin valor tenia la variable a abans  
+// d’executar-se la instrucció? 
+//  I desprès? 
+console.log(a); 
+b = b + 4 – a 
+console.log(b); 
+c = a + b + c 
+console.log(c); 
+a = a + c 
+console.log(a); 
+b = 4 
+console.log(b); 
+c = c + 3 - b + 2 
+console.log(c);
